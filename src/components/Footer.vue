bee2bf82cae4bd05dec4f3fd8427662c8c0dbf75
ElenaRuscitto <ruscitto.elena@gmail.com>
2024-03-25 14:52:02 +0100

    scaffloiding

---
+++ src/components/Footer.vue
@@ -0,0 +1,18 @@
+<script>
+
+</script>
+
+
+<template>
+  <div>
+    Footer
+  </div>
+  
+</template>
+
+
+<style lang="scss" scoped>
+@use '../assets/scss/partials/general' as *;
+@use '../assets/scss/partials/variables' as *;
+
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,17 @@
+<script>
+
+</script>
+
+
+<template>
+  <div>
+    header
+  </div>
+</template>
+
+
+<style lang="scss" scoped>
+@use '../assets/scss/partials/general' as *;
+@use '../assets/scss/partials/variables' as *;
+
+</style>
